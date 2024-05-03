034d9f07fefbc3e4bcd193d90bf4594805e47998
tim1007 <111577841+yuzunsang@users.noreply.github.com>
2024-05-03 18:14:52 +0900

    tailwind start

---
+++ src/components/Header.jsx
@@ -0,0 +1,109 @@
+export default function Header() {
+  return (
+    <header className="bg-[#1c1c1e] text-white flex justify-between items-center p-4">
+      <div className="flex items-center space-x-4">
+        <MenuIcon className="w-6 h-6" />
+        <span className="font-bold text-lg">LOGO</span>
+        <div variant="secondary">Beta</div>
+      </div>
+      <div className="flex items-center space-x-4">
+        <button
+          className="border-white border-opacity-50 text-white"
+          variant="outline"
+        >
+          스트리머, 게임 영상 검색
+        </button>
+        <SearchIcon className="w-6 h-6" />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Grid2x2Icon className="w-6 h-6" />
+        <BellIcon className="w-6 h-6" />
+        <a className="text-white" href="#">
+          로그인
+        </a>
+      </div>
+    </header>
+  );
+}
+
+function BellIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+      <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+    </svg>
+  );
+}
+
+function Grid2x2Icon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="18" height="18" x="3" y="3" rx="2" />
+      <path d="M3 12h18" />
+      <path d="M12 3v18" />
+    </svg>
+  );
+}
+
+function MenuIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  );
+}
+
+function SearchIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="11" cy="11" r="8" />
+      <path d="m21 21-4.3-4.3" />
+    </svg>
+  );
+}
